dc6ef85841b09a3045ccb22a38ad51f24ad4d187
Luis Sanchez <sanchezl@redhat.com>
2023-03-20 10:38:36 -0400

    use env prefix to avoid collisions

--- cmd/root.go
+++ cmd/root.go
@@ -68,6 +68,7 @@ func initConfig() {
 		viper.SetConfigName("config")
 	}
 
+	viper.SetEnvPrefix("OSAL")
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
